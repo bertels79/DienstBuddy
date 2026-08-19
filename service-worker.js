59028fd7c4eeabb7e46ef24ad49df5bf387c5164
bertels79 <bertels79@gmail.com>
2026-08-19 10:16:23 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,4 @@
-const C='dienstbuddy-v2';self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(['./','./index.html','./manifest.webmanifest']))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
+const CACHE='dienstbuddy-v2-5';
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>{if(e.request.method==='GET')e.respondWith(fetch(e.request,{cache:'no-store'}).catch(()=>caches.match(e.request)))});
